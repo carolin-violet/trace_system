4f5c6629375d60abee7b1ffe0990473ddbab0f72
zj-fight <2976008778@qq.com>
2022-02-15 11:14:51 +0800

    commit2

--- src/app_init.py
+++ src/app_init.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from models import db
-from src.views import login, userInfo, articles, plans, diary, navigations
+from src.views import login, userInfo
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('settings.Config')
 
-
-    app.secret_key = 'zjyyds'
+    app.secret_key = 'yesyyds'
 
     db.app = app
     db.init_app(app)
@@ -18,10 +17,7 @@ def create_app():
 
     app.register_blueprint(login.api)
     app.register_blueprint(userInfo.api)
-    app.register_blueprint(articles.api)
-    app.register_blueprint(plans.api)
-    app.register_blueprint(diary.api)
-    app.register_blueprint(navigations.api)
+
     return app
 
 
--- src/models.py
+++ src/models.py
@@ -8,49 +8,97 @@ db = SQLAlchemy()
 # 用户基本信息表
 class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # id作为用户id
-    nickName = db.Column(db.String(10), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(10), nullable=False)
     phone = db.Column(db.String(11), unique=True)  # 手机号码,用作登录账号,用作主键
     password = db.Column(db.String(20), nullable=False)
     gender = db.Column(db.String(6), nullable=False)  # 性别:male/female
-    email = db.Column(db.String(20), nullable=False)  # 电子邮箱
+
+    def __init__(self, userName, phone, password, gender):
+        self.userName = userName
+        self.phone = phone
+        self.password = password
+        self.gender = gender
+
+    def __repr__(self):
+        return '<User %r>' % self.userName
 
 
-# 文章信息表
-class Article(db.Model):
-    __tablename__ = "articles"
-    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True)  # id
-    article_id = db.Column(db.Integer, nullable=False)  # 文章id
-    title = db.Column(db.String(30), nullable=False)  # 标题
-    author = db.Column(db.String(20), nullable=False)  # 作者
-    paragraph_id = db.Column(db.Integer, nullable=False)  # 段落
-    content = db.Column(db.Text, nullable=False)  # 内容
+# 商品信息表
+class Commodity(db.Model):
+    __tablename__ = 'commodity'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(10), nullable=False)  # 商品名称
+    price = db.Column(db.Integer, nullable=False)  # 商品单价
+    weight = db.Column(db.String(10), nullable=True)  # 商品重量
+    status = db.Column(db.String(10), nullable=False)  # 商品状态
+    product_id = db.Column(db.String(255), nullable=False)  # 编号（ 物流：运单号  仓库：仓库编号）
+    block_info = db.Column(db.Text, default='')  # 二维码信息
+
+    def __init__(self, name, price, weight, status, product_id, block_info=''):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.status = status
+        self.product_id = product_id
+        self.block_info = block_info
+
+    def __repr__(self):
+        return '<Commodity %r>' % self.name
 
 
-# 计划信息表
-class Plan(db.Model):
-    __tablename__ = "plans"
-    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True)  # id
-    startTime = db.Column(db.DateTime, nullable=False)  # 开始时间
-    endTime = db.Column(db.DateTime, nullable=False)  # 截止时间
-    content = db.Column(db.String(50), nullable=False)  # 内容
-    completed = db.Column(db.Integer, default=0)  # 完成情况
+# 运输信息
+class Transport(db.Model):
+    __tablename__ = 'transport'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.String(255), nullable=False)  # 编号（ 物流：运单号  仓库：仓库编号）
+    com = db.Column(db.String(50), nullable=False)  # 操作的公司名
+    time = db.Column(db.String(100), nullable=False)  # 操作时间，需要获取当前系统时间
+    ini = db.Column(db.String(100), nullable=False)  # 初始地
+    des = db.Column(db.String(100), nullable=False)  # 目的地
+    person = db.Column(db.String(20), nullable=False)  # 操作人
+    tel = db.Column(db.String(30), nullable=False)  # 操作人联系方式
+
+    def __init__(self, product_id, com, time, ini, des, person, tel):
+        self.product_id = product_id
+        self.com = com
+        self.time = time
+        self.ini = ini
+        self.des = des
+        self.person = person
+        self.tel = tel
 
 
-# 笔记信息表
-class Diary(db.Model):
-    __tablename__ = "diaries"
-    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True)  # id
-    date = db.Column(db.DateTime, nullable=False)  # 日期
-    content = db.Column(db.Text, nullable=False)  # 内容
+# 农产品温湿度信息 (时间充足再来完善)
+class Detail(db.Model):
+    __tablename__ = 'detail'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    temp = db.Column(db.FLOAT, nullable=False)  # 温度
+    hum = db.Column(db.FLOAT, nullable=False)  # 湿度
+    date = db.Column(db.String(100), nullable=False)  # 时间
+
+    def __init__(self, temp, hum, date):
+        self.temp = temp
+        self.hum = hum
+        self.date = date
 
 
-# 导航信息表
-class Navigation(db.Model):
-    __tablename__ = "navigations"
-    id = db.Column(db.BIGINT, primary_key=True, autoincrement=True)  # id
-    category = db.Column(db.String(10), nullable=False)
-    category_id = db.Column(db.Integer, nullable=False)
-    title = db.Column(db.String(20), nullable=False)
-    url = db.Column(db.String(100), nullable=False)
-    img_url = db.Column(db.String(100), default="")
+# 区块链信息
+class Blockchain(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    index = db.Column(db.String(100), nullable=False)  # 索引
+    commodity_id = db.Column(db.Integer, nullable=False)
+    commodity_name = db.Column(db.String(100), nullable=False)
+    data = db.Column(db.Text, nullable=True)
+    pre_hash = db.Column(db.String(255), nullable=True)
+    cur_hash = db.Column(db.String(255), nullable=True)
+    nonce = db.Column(db.String(255), nullable=True)  # 随机数
+
+    def __init__(self, index, commodity_id, commodity_name, data, prehash, curhash, nonce):
+        self.index = index
+        self.commodity_id = commodity_id
+        self.commodity_name = commodity_name
+        self.data = data
+        self.prehash = prehash
+        self.curhash = curhash
+        self.nonce = nonce

be92e3527a9a2bc87148101ba2862477e1566243
zj-fight <2976008778@qq.com>
2022-02-16 11:31:34 +0800

    commit7

--- src/views/logistics.py
+++ src/views/logistics.py
@@ -7,6 +7,26 @@ from flask import Blueprint
 logistics_page = Blueprint('logistics_page', __name__)
 
 
+# 添加物流信息
 @logistics_page.route('/logistics', methods=['POST'])
-def logistics():
+def add_logistics():
     pass
+
+
+# 删除物流信息
+@logistics_page.route('/logistics/<product_id>', methods=['DELETE'])
+def del_logistics(product_id):
+    pass
+
+
+# 修改物流信息
+@logistics_page.route('/logistics/<product_id>', methods=['PATCH'])
+def modify_logistics(product_id):
+    pass
+
+
+# 查询物流信息
+@logistics_page.route('/logistics/<product_id>', methods=['GET'])
+def query_logistics(product_id):
+    pass
+

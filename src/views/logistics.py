585531b33cade6ee999680855538e222ae268677
zj-fight <2976008778@qq.com>
2022-02-20 10:17:16 +0800

    commit12

--- src/views/logistics.py
+++ src/views/logistics.py
@@ -2,9 +2,10 @@
 物流信息模块
 """
 from src.models import Logistics, Blockchain, db
-from src.utils import chain
+from src.utils import chain, img
 from flask import Blueprint, request, jsonify
 import time
+import json
 
 logistics_page = Blueprint('logistics_page', __name__)
 
@@ -25,14 +26,41 @@ def add_logistics():
     if Logistics.query.filter(Logistics.product_id == product_id).first():
         return '物流已存在'
     else:
+        '''
+        添加物流
+        '''
+        logistics = Logistics(product_id, status, com, cur_time, ini, des, cur, person, tel)
+        db.session.add(logistics)
+        db.session.commit()
+
+        '''
+        添加区块
+        '''
         block = chain.Chain(product_id)
         blocks = Blockchain.query.filter(Blockchain.commodity_id == product_id).all()
         if not blocks:
             block.create_genesis_block(db)
-        block.add_block(db, product_id, status, com, cur_time, ini, des, cur, person, tel)  # 添加区块信息
-        logistics = Logistics(product_id, status, com, cur_time, ini, des, cur, person, tel)
-        db.session.add(logistics)
-        db.session.commit()
+        block.add_block(db, product_id, status, com, cur_time, ini, des, cur, person, tel)
+
+        '''
+        添加二维码信息
+        '''
+        data = {
+            "商品id": product_id,
+            "始发地": ini,
+            "目的地": des,
+            "运输信息": []
+        }
+        for item in blocks[1:-1]:
+            data['运输信息'].append({
+                "时间": item.time,
+                "状态": item.status,
+                "操作公司": item.com,
+                "当前所在地": item.cur,
+                "操作人": item.person,
+                "操作人电话": item.tel
+            })
+        img.make_qrcode(product_id, json.dumps(data, sort_keys=True))
         return '添加成功'
 
 

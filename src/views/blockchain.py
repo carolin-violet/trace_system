b9f7ec24977f3be7b0987ef8ebd375fd42bf468b
zj-fight <2976008778@qq.com>
2022-03-14 15:34:05 +0800

    commit13

--- src/views/blockchain.py
+++ src/views/blockchain.py
@@ -1,10 +1,10 @@
 """
 区块链api模块
 """
-from src.models import Blockchain, db
+from flask import Blueprint, jsonify, request
+from src.models import Blockchain, db, Logistics, Commodity
 from src.utils import chain
-from flask import Blueprint, jsonify
-import json
+from src.security import RSA
 
 
 chain_page = Blueprint('chain_page', __name__)
@@ -17,8 +17,54 @@ chain_page = Blueprint('chain_page', __name__)
 
 @chain_page.route('/blockchain', methods=['POST'])
 def add_block():
+    logistics_id = request.json['logistics_id']
+    summary = {}
 
-    return str(chains)
+    '''
+    汇总物流信息
+    '''
+    logistics_s = Logistics.query.filter(Logistics.logistics_id == logistics_id).all()
+    logistics_data = []
+    for lgs in logistics_s:
+        logistics_data.append({
+            "status": lgs.status,
+            "com": lgs.com,
+            "time": lgs.time,
+            "cur": lgs.cur,
+            "person": lgs.person,
+            "tel": lgs.tel,
+        })
+
+    '''
+    汇总商品信息
+    '''
+    commodity = Commodity.query.filter(Commodity.logistics_id == logistics_id).first()
+    commodity_data = {
+        "user_id": commodity.user_id,
+        "area_id": commodity.area_id,
+        "batch": commodity.batch,
+        "name": commodity.name,
+        "price": commodity.price,
+        "weight": commodity.weight,
+        "logistics_id": commodity.logistics_id,
+        "ini": commodity.ini,
+        "des": commodity.des,
+        "qrcode_url": commodity.qrcode_url
+    }
+
+    '''
+    汇总存储信息
+    '''
+
+    '''
+    汇总生产信息
+    '''
+
+
+    blocks = Blockchain.query.all()
+    blockchain = chain.Chain(blocks, db)
+
+    return '添加成功'
 
 
 '''
@@ -26,28 +72,20 @@ def add_block():
 '''
 
 
-@chain_page.route('/blockchain/<commodity_id>', methods=['GET'])
-def query_chain(commodity_id):
-    blocks = Blockchain.query.filter(Blockchain.commodity_id == commodity_id).all()
+@chain_page.route('/blockchain', methods=['GET'])
+def query_chain():
+    blocks = Blockchain.query.all()
 
-    data_1 = json.loads(blocks[1].data, strict=False)
-    data = {
-        "商品id": commodity_id,
-        "始发地": data_1["ini"],
-        "目的地": data_1["des"],
-        "运输信息": []
-    }
+    data = []
     for block in blocks:
-        if block.index != 0:
-            detail_data = json.loads(block.data, strict=False)
-            data['运输信息'].append({
-                "时间": detail_data["time"],
-                "状态": detail_data["status"],
-                "操作公司": detail_data["com"],
-                "当前所在地": detail_data["cur"],
-                "操作人": detail_data["person"],
-                "操作人电话": detail_data["tel"]
-            })
+        data.append({
+            "logistics_id": block.logistics_id,
+            "cur_hash": block.cur_hash,
+            "pre_hash": block.pre_hash,
+            "timestamp": block.timestamp,
+            "nonce": block.nonce,
+            "data": block.data,
+        })
     return jsonify(data)
 
 
@@ -56,9 +94,9 @@ def query_chain(commodity_id):
 '''
 
 
-@chain_page.route('/blockchain/validate_proof/<commodity_id>', methods=['GET'])
+@chain_page.route('/blockchain/validate_proof', methods=['GET'])
 def validate_chain(commodity_id):
-    blocks = Blockchain.query.filter(Blockchain.commodity_id == commodity_id).all()
+    blocks = Blockchain.query.all()
     verify_chain = chain.Chain(commodity_id, blocks)
     is_correct = verify_chain.validate_chain
     if is_correct:

eee9d7d338ddccca37a7760f6173e419cda42ae9
zj-fight <2976008778@qq.com>
2022-03-14 18:31:53 +0800

    commit13

--- src/views/commodity.py
+++ src/views/commodity.py
@@ -19,17 +19,17 @@ commodity_page = Blueprint('commodity_page', __name__)
 @commodity_page.route('/commodity', methods=['POST'])
 def add_commodity():
     user_id = request.form['user_id']
-    area_id = request.form['area_id']
-    batch = request.form['batch']
+    area_id = int(request.form['area_id'])
+    batch = int(request.form['batch'])
     name = request.form['name']
-    price = request.form['price']
-    weight = request.form['weight']
-    logistics_id = uuid1()
+    price = float(request.form['price'])
+    weight = float(request.form['weight'])
+    logistics_id = str(uuid1())
     ini = request.form['ini']
     des = request.form['des']
 
-    qrcode_url = "http://127.0.0.1:5000" + "/commodity/" + str(logistics_id)
-    qr_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'static/qr_codes/'+str(logistics_id)+'.png'))
+    qrcode_url = "http://127.0.0.1:5000" + "/purchase/" + str(logistics_id)
+    qr_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/static/qr_codes/'+logistics_id+'.png'
     img.make_qrcode(qrcode_url, qr_path)
 
     commodity = Commodity(user_id, area_id, batch, name, price, weight, logistics_id, ini, des, qrcode_url)
@@ -44,9 +44,9 @@ def add_commodity():
 '''
 
 
-@commodity_page.route('/commodity/<cur_hash>', methods=['DELETE'])
-def del_commodity(cur_hash):
-    commodity = Commodity.query.filter(Commodity.cur_hash == cur_hash).first()
+@commodity_page.route('/commodity/<logistics_id>', methods=['DELETE'])
+def del_commodity(logistics_id):
+    commodity = Commodity.query.filter(Commodity.logistics_id == logistics_id).first()
     if commodity:
         db.session.delete(commodity)
         db.session.commit()
@@ -72,11 +72,10 @@ def query_commodity():
             'name': commodity.name,
             'price': commodity.price,
             'weight': commodity.weight,
-            'commodity_id': commodity.commodity_id,
+            'logistics_id': commodity.logistics_id,
             'ini': commodity.ini,
             'des': commodity.des,
-            'qrcode_url': commodity.qrcode_url,
-            'cur_hash': commodity.cur_hash
+            'qrcode_url': commodity.qrcode_url
         })
     return jsonify(data)
 
@@ -86,9 +85,9 @@ def query_commodity():
 '''
 
 
-@commodity_page.route('/commodity/<cur_hash>', methods=['GET'])
-def get_commodity(cur_hash):
-    commodity = Commodity.query.filter(Commodity.cur_hash == cur_hash).first()
+@commodity_page.route('/commodity/<logistics_id>', methods=['GET'])
+def get_commodity(logistics_id):
+    commodity = Commodity.query.filter(Commodity.logistics_id == logistics_id).first()
     if commodity:
         return jsonify({
             'user_id': commodity.user_id,
@@ -97,11 +96,10 @@ def get_commodity(cur_hash):
             'name': commodity.name,
             'price': commodity.price,
             'weight': commodity.weight,
-            'commodity_id': commodity.commodity_id,
+            'logistics_id': commodity.logistics_id,
             'ini': commodity.ini,
             'des': commodity.des,
-            'qrcode_url': commodity.qrcode_url,
-            'cur_hash': commodity.cur_hash
+            'qrcode_url': commodity.qrcode_url
         })
     else:
         return '商品不存在'
@@ -114,7 +112,7 @@ def get_commodity(cur_hash):
 
 @commodity_page.route('/commodity/img/<logistics_id>', methods=['GET'])
 def get_img(logistics_id):
-    img_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'static/qr_codes/'+str(logistics_id)+'.png'))
+    img_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/static/qr_codes/'+logistics_id+'.png'
     return send_file(img_path, mimetype='image/gif')
 
 

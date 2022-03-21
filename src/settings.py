d8b53da08bfc9adf65b703a8bb547c7be85ff655
zj-fight <2976008778@qq.com>
2022-03-21 20:20:21 +0800

    commit14

--- src/settings.py
+++ src/settings.py
@@ -18,10 +18,10 @@ class Config(object):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True  # 修改数据模型后自动执行，不需要commit()
-    SQLALCHEMY_POOL_SIZE = 5  # 数据库连接池的大小。默认是数据库引擎的默认值 （通常是 5）
+    SQLALCHEMY_POOL_SIZE = 100  # 数据库连接池的大小。默认是数据库引擎的默认值 （通常是 5）
     SQLALCHEMY_POOL_TIMEOUT = 10  # 指定数据库连接池的超时时间。默认是 10
     # 自动回收连接的秒数。这对 MySQL 是必须的，默认 情况下 MySQL 会自动移除闲置 8 小时或者以上的连接。 需要注意地是如果使用 MySQL 的话， Flask-SQLAlchemy 会自动地设置这个值为 2 小时
-    SQLALCHEMY_POOL_RECYCLE = 0.001
+    SQLALCHEMY_POOL_RECYCLE = 3600
 
 
     # # Redis的配置
